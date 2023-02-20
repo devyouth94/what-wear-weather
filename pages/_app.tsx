a022a147bf2bcad88644041961fe551d43436b8f
devyouth94 <devyouth94@gmail.com>
2023-02-20 23:40:33 +0900

    Feat: react-query 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
-import '@/styles/globals.css';
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
+import '@/styles/globals.css';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { refetchOnWindowFocus: false } },
+      }),
+  );
+
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
