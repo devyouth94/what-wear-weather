ca52e52a446dc8da73730d98340f64e8548de1d5
devyouth94 <devyouth94@gmail.com>
2023-02-25 17:55:58 +0900

    Refactor: 코드 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
-import '@/styles/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ChakraProvider } from '@chakra-ui/react';
+
 import theme from '@/styles/theme';
 import tailwindThemeToggle from '@/styles/tailwindThemeToggle';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(
