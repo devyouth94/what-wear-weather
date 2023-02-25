71fe2debb83d16e0aff6c8fa7e94bb7b9da71638
devyouth94 <devyouth94@gmail.com>
2023-02-25 17:54:29 +0900

    Refactor: 첫 페이지 구현
    로그인 여부에 따라 로그인화면 혹은 메인화면으로 리다이렉트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+
 import FullLogo from '@/components/common/FullLogo';
+import Layout from '@/components/common/Layout';
 
 export default function Home() {
+  const { status } = useSession();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (status === 'loading') return;
+
+    const timer = setTimeout(() => {
+      if (status === 'authenticated') {
+        push('/main');
+      }
+
+      if (status === 'unauthenticated') {
+        push('/login');
+      }
+    }, 2500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [status]);
+
   return (
-    <main>
+    <Layout className="flex justify-center items-center">
       <FullLogo />
-    </main>
+    </Layout>
   );
 }
