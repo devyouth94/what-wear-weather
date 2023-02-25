e1698ddf00f00d2e4b22b3c5f7ecaa9eb4912a32
devyouth94 <devyouth94@gmail.com>
2023-02-25 13:48:05 +0900

    Feat: 다크 모드 설정
    tailwind css & chakra ui 전부 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 import '@/styles/globals.css';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from '@/styles/theme';
+import tailwindThemeToggle from '@/styles/tailwindThemeToggle';
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(
@@ -14,8 +16,12 @@ export default function App({ Component, pageProps }: AppProps) {
       }),
   );
 
+  useEffect(() => {
+    tailwindThemeToggle();
+  }, []);
+
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <SessionProvider session={pageProps.session}>
           <Component {...pageProps} />
