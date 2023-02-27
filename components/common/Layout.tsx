f711bcda3a855c65679ab29865c9ad2cc4b3facb
devyouth94 <devyouth94@gmail.com>
2023-02-28 01:29:49 +0900

    Refactor: 기본 구조 컴포넌트 생성 및 수정
    헤더, 레이아웃, 네비게이션 컴포넌트

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,17 +1,21 @@
-import { motion, AnimatePresence } from 'framer-motion';
+import { CSSProperties } from 'react';
 import { useRouter } from 'next/router';
+import { motion, AnimatePresence } from 'framer-motion';
 
 type Props = {
   children: React.ReactNode;
   className?: string;
+  style?: CSSProperties;
 };
 
-const Layout = ({ children, className }: Props) => {
+const Layout = ({ children, className, style }: Props) => {
   const { route } = useRouter();
 
   return (
     <AnimatePresence mode="wait">
-      <main className="w-full md:w-[375px] md:translate-x-1/2 px-5 bg-neutral-100">
+      <main
+        style={style}
+        className="w-full min-w-[375px] md:absolute md:w-[375px] md:left-1/2 md:-translate-x-1/2 px-5 bg-neutral-100 cursor-default">
         <motion.div
           key={route}
           initial={{ opacity: 0 }}
--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -1,12 +1,32 @@
+import { IconHome, IconUser } from '@/static/Icons';
 import { useRouter } from 'next/router';
 
+const className = `
+  fixed bottom-0
+  flex justify-center items-baseline
+  w-full min-w-[375px] h-[75px]
+  md:w-[375px] md:left-1/2 md:-translate-x-1/2
+  bg-transparent
+  z-20
+`;
+
 const Nav = () => {
-  const { push } = useRouter();
+  const { push, route } = useRouter();
 
   return (
-    <nav className="fixed bottom-0 w-full md:w-[375px] md:translate-x-1/2 h-[60px] flex gap-3 justify-center items-center bg-neutral-300">
-      <button onClick={() => push('/main')}>홈</button>
-      <button onClick={() => push('/mypage')}>마이페이지</button>
+    <nav className={className}>
+      <div className="flex justify-center items-center gap-5 px-5 py-3 bg-neutral-50 rounded-full shadow-md">
+        <IconHome
+          className="cursor-pointer"
+          onClick={() => push('/main')}
+          fill={route === '/main' ? '#b03232' : 'gray'}
+        />
+        <IconUser
+          className="cursor-pointer"
+          onClick={() => push('/mypage')}
+          fill={route === '/mypage' ? '#b03232' : 'gray'}
+        />
+      </div>
     </nav>
   );
 };
