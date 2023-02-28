5e976ccb86b834949b1064edfd8ae1e58c73b774
devyouth94 <devyouth94@gmail.com>
2023-02-28 22:02:47 +0900

    Refactor: 로고 커서 포인터 수정

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,7 +3,10 @@ import SimpleLogo from '@/components/common/SimpleLogo';
 const Header = () => {
   return (
     <div className="flex justify-center">
-      <SimpleLogo className="h-[20px] drop-shadow-md" onClick={() => window.location.reload()} />
+      <SimpleLogo
+        className="h-[20px] drop-shadow-md cursor-pointer"
+        onClick={() => window.location.reload()}
+      />
     </div>
   );
 };
