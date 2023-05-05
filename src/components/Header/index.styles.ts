72fd68c5d2ea3cf7b0223df3077bf0755ae93945
devyouth94 <devyouth94@gmail.com>
2023-05-05 20:18:24 +0900

    Refactor: 헤더 컴포넌트 수정

---
+++ src/components/Header/index.styles.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 60px;
+
+  svg {
+    width: 112px;
+  }
+`;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import SimpleLogo from '@/elements/SimpleLogo';
+
+import * as S from './index.styles';
+
+const Header = () => {
+  return (
+    <S.Header>
+      <SimpleLogo />
+    </S.Header>
+  );
+};
+
+export default Header;
