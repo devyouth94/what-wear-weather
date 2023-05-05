1c3fd4f6e5b4e3efd6cfa8150272c56718c2544c
devyouth94 <devyouth94@gmail.com>
2023-05-06 00:22:32 +0900

    Refactor: 배경 이미지 저작권 컴포넌트 수정

---
+++ src/components/ImageAuthor/index.tsx
@@ -0,0 +1,26 @@
+import Text from '@/elements/Text';
+import { useBackgroundImageState } from '@/stores/useBackgroundImageStore';
+
+import * as S from './index.styles';
+
+const ImageAuthor = () => {
+  const { profile, name, unsplash } = useBackgroundImageState();
+
+  return (
+    <S.Container>
+      <Text variant="caption">
+        <span>Photo by </span>
+        <a href={profile} target="_blank" rel="noreferrer">
+          {name}
+        </a>
+        <span> on </span>
+        <a href={unsplash} target="_blank" rel="noreferrer">
+          Unsplash
+        </a>
+      </Text>
+      ;
+    </S.Container>
+  );
+};
+
+export default ImageAuthor;
