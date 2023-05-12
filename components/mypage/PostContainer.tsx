9d6daebc08cb4108c85743e475c33e4a54c5a918
devyouth94 <devyouth94@gmail.com>
2023-05-12 20:50:46 +0900

    Refactor: 레거시 파일 삭제

--- components/mypage/PostContainer.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-  children: React.ReactNode;
-};
-
-const PostContainer = ({ children }: Props) => {
-  return <div className="grid grid-cols-2 items-start gap-2 mt-5 h-full">{children}</div>;
-};
-
-export default PostContainer;
