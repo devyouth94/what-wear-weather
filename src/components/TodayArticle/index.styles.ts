87486b08ce240b748877ef22255875d9c396fd1b
devyouth94 <devyouth94@gmail.com>
2023-05-12 16:54:16 +0900

    Refactor: 오늘의 옷 컴포넌트 수정 완료

---
+++ src/components/TodayArticle/index.styles.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const TodayWear = styled.div`
+  position: relative;
+
+  & > span {
+    position: absolute;
+    bottom: 10px;
+    left: 10px;
+    z-index: 9;
+  }
+`;
--- src/components/TodayArticle/index.tsx
+++ src/components/TodayArticle/index.tsx
@@ -1,6 +1,11 @@
+import Text from '@/elements/Text';
 import BasicButton from '@/elements/BasicButton';
+import ArticleItem from '@/components/ArticleItem';
+
 import useGetTodayArticle from '@/hooks/main/useGetTodayArticle';
 import { useDrawerActions } from '@/stores/useDrawerStore';
+
+import * as S from './index.styles';
 
 const TodayArticle = () => {
   const { data } = useGetTodayArticle();
@@ -12,10 +17,15 @@ const TodayArticle = () => {
 
   return (
     <>
-      {data?.ok ? (
+      {data && data.ok && (
         <BasicButton onClick={handleClickWriteButton}>오늘의 옷 등록하기</BasicButton>
-      ) : (
-        <div></div>
+      )}
+
+      {data && data.result && (
+        <S.TodayWear>
+          <Text variant="head_02">Today&apos;s Wear</Text>
+          <ArticleItem {...data.result} />
+        </S.TodayWear>
       )}
     </>
   );
