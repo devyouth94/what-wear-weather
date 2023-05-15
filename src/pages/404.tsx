f384f0dbcc6d87f07a0ef5002a4633ac8b55bfe9
devyouth94 <devyouth94@gmail.com>
2023-05-16 00:48:40 +0900

    Refactor: 404페이지 수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,20 @@
 // import Layout from '@/components/common/Layout';
-import { Button } from '@chakra-ui/react';
 import Link from 'next/link';
+
+import BasicButton from '@/elements/BasicButton';
+import Layout from '@/elements/Layout';
+import Text from '@/elements/Text';
 
 const NotFound = () => {
   return (
-    <div>ㅇㅇ</div>
-    // <Layout className="flex flex-col gap-5 justify-center items-center">
-    //   <span className="font-black text-lg">잘못 오셨군요!</span>
-
-    //   <Link href="/">
-    //     <Button colorScheme="red">홈으로</Button>
-    //   </Link>
-    // </Layout>
+    <Layout center>
+      <Text variant="head_01">Error!</Text>
+      <Link href="/" style={{ width: '100%' }}>
+        <BasicButton variant="outline" fontColor="secondary_01" color="secondary_01">
+          홈으로
+        </BasicButton>
+      </Link>
+    </Layout>
   );
 };
 
