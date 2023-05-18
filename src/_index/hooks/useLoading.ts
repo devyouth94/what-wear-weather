d729e5022d2b0428ac42e8212e25d370905cf9c3
devyouth94 <devyouth94@gmail.com>
2023-05-18 17:33:55 +0900

    Refactor: useLoading hooks 생성

---
+++ src/_index/hooks/useLoading.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+
+const useLoading = () => {
+  const { status } = useSession();
+  const { push } = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(true);
+
+      // if (status === 'authenticated') {
+      //   push('/main');
+      // }
+
+      // if (status === 'unauthenticated') {
+      //   push('/login');
+      // }
+    }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [status]);
+
+  return isLoading;
+};
+
+export default useLoading;
