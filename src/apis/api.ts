126e24a4de9f0a2d29bb8099192e6d4c562364b6
devyouth94 <devyouth94@gmail.com>
2023-05-19 01:08:49 +0900

    Refactor: api 코드 수정

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 import type { TGetArticle, TPostArticle, TPostProfile } from '@/types/articleTypes';
 
 const api = axios.create({
@@ -7,13 +8,11 @@ const api = axios.create({
 
 export const getTodayArticle = async () => {
   const { data } = await api.get<{ result?: TGetArticle; ok: boolean }>('/api/files/today');
-
   return data;
 };
 
 export const getArticles = async () => {
   const { data } = await api.get<{ result: TGetArticle[] }>('/api/files');
-
   return data.result;
 };
 
@@ -21,7 +20,6 @@ export const getArticle = async (articleId: string | null) => {
   const { data } = await api.get<{ result: TGetArticle }>('/api/files', {
     params: { articleId },
   });
-
   return data.result;
 };
 
@@ -29,13 +27,11 @@ export const getArticlesBySearch = async (temp: number[]) => {
   const { data } = await api.get<{ result: TGetArticle[] }>('/api/files', {
     params: { min: temp[0], max: temp[1] },
   });
-
   return data.result;
 };
 
 export const postArticle = async (file: TPostArticle) => {
   const formData = new FormData();
-
   formData.append('region', file.region);
   formData.append('temp_now', String(file.temp_now));
   formData.append('temp_feels', String(file.temp_feels));
@@ -45,19 +41,16 @@ export const postArticle = async (file: TPostArticle) => {
   formData.append('image', file.image);
 
   const { data } = await api.post('/api/files', formData);
-
   return data;
 };
 
 export const deleteArticle = async (postId: string) => {
   const { data } = await api.delete(`/api/files?postId=${postId}`);
-
   return data;
 };
 
 export const postProfile = async (profile: TPostProfile) => {
   const formData = new FormData();
-
   formData.append('nickname', profile.nickname);
   if (profile.image) {
     formData.append('image', profile.image);
@@ -65,6 +58,5 @@ export const postProfile = async (profile: TPostProfile) => {
 
   const { data } = await api.post('/api/auth/profile', formData);
   await api.get('/api/auth/session?update');
-
   return data;
 };
