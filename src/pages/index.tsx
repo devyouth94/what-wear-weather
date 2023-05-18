133a743a2d7a2c19a0cd979cd979c5350087f657
devyouth94 <devyouth94@gmail.com>
2023-05-18 17:36:16 +0900

    Refactor: 초기 페이지 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,22 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { useSession } from 'next-auth/react';
+import FullLogo from '@/_index/elements/FullLogo';
+import useLoading from '@/_index/hooks/useLoading';
 
-import Layout from '@/elements/Layout';
-import FullLogo from '@/elements/FullLogo';
+import Layout from '@/@shared/elements/Layout';
+import Overlay from '@/@shared/elements/Overlay';
+import Spinner from '@/@shared/elements/Spinner';
 
 export default function Home() {
-  const { status } = useSession();
-  const { push } = useRouter();
-
-  const [isOpen, setIsOpen] = useState(false);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsOpen(true);
-
-      if (status === 'authenticated') {
-        push('/main');
-      }
-
-      if (status === 'unauthenticated') {
-        push('/login');
-      }
-    }, 2500);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [status]);
+  const isLoading = useLoading();
 
   return (
     <>
       <Layout center>
+        {isLoading && (
+          <>
+            <Spinner color="point_01" />
+            <Overlay />
+          </>
+        )}
         <FullLogo />
       </Layout>
     </>
