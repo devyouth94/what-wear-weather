37c0d4f5d03615cc0137b4244a04eea5778f74fc
devyouth94 <devyouth94@gmail.com>
2023-05-19 01:09:42 +0900

    Refactor: 메인 페이지 수정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,18 +2,19 @@ import type { GetServerSideProps } from 'next';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]';
 
-import WriteDrawer from '@/components/WriteDrawer';
-import Layout from '@/elements/Layout';
-import Header from '@/components/Header';
-import LiveWeather from '@/components/LiveWeather';
-import TodayArticle from '@/components/TodayArticle';
-import DailyWeather from '@/components/DailyWeather';
-import ImageAuthor from '@/components/ImageAuthor';
-import Nav from '@/components/Nav';
+import DailyWeather from '@/_main/components/DailyWeather';
+import ImageAuthor from '@/_main/components/ImageAuthor';
+import LiveWeather from '@/_main/components/LiveWeather';
+import TodayArticle from '@/_main/components/TodayArticle';
+import WriteDrawer from '@/_main/components/WriteDrawer';
+import LogoHeader from '@/_main/elements/LogoHeader';
+import useGeolocation from '@/_main/hooks/useGeolocation';
 
-import useGeolocation from '@/hooks/common/useGeolocation';
+import ArticleDrawer from '@/@shared/components/ArticleDrawer';
+import Layout from '@/@shared/elements/Layout';
+import Nav from '@/@shared/elements/Nav';
+
 import { useBackgroundImageState } from '@/stores/useBackgroundImageStore';
-import ArticleDrawer from '@/components/ArticleDrawer';
 
 const Main = () => {
   const { location } = useGeolocation();
@@ -25,14 +26,10 @@ const Main = () => {
       <WriteDrawer location={location} />
 
       <Layout backgroundImage={src}>
-        <Header />
-
+        <LogoHeader />
         <LiveWeather location={location} />
-
         <TodayArticle />
-
         <DailyWeather location={location} />
-
         <ImageAuthor />
       </Layout>
 
