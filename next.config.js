e8e12cf3136b29525786fe5c292b87e9a6bf76a7
devyouth94 <devyouth94@gmail.com>
2023-05-19 17:39:24 +0900

    Feat: PWA 적용

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa');
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
   reactStrictMode: true,
   images: {
     domains: [`${process.env.BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com`, 'openweathermap.org'],
   },
 };
+
+const nextConfig = withPWA({
+  dest: 'public',
+  disable: !isProduction,
+  runtimeCaching: [],
+})(config);
 
 module.exports = nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <title>왓웨어웨더. | 그날의 옷을 저장하세요</title>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#2C3639" />
+      </Head>
       <body>
         <Main />
         <NextScript />
