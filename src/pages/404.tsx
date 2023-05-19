47f3d470a0a9b3bbf36a7c563fc45fe6d59235c4
devyouth94 <devyouth94@gmail.com>
2023-05-19 16:55:39 +0900

    Refactor: 404 수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,26 +1,35 @@
 // import Layout from '@/components/common/Layout';
 import Link from 'next/link';
+import styled from '@emotion/styled';
 
 import LogoHeader from '@/_main/elements/LogoHeader';
 
 import BasicButton from '@/@shared/elements/BasicButton';
-import Text from '@/@shared/elements/Text';
-
-import * as S from './index.styles';
 import Layout from '@/@shared/elements/Layout';
+import Text from '@/@shared/elements/Text';
 
 const NotFound = () => {
   return (
     <Layout center>
-      <S.LayoutNotFound>
+      <LayoutNotFound>
         <LogoHeader />
         <Text variant="head_04">문제가 있네요</Text>
         <BasicButton variant="outline" fontColor="secondary_01" color="secondary_01">
           <Link href="/">홈으로</Link>
         </BasicButton>
-      </S.LayoutNotFound>
+      </LayoutNotFound>
     </Layout>
   );
 };
 
 export default NotFound;
+
+export const LayoutNotFound = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  width: 100%;
+`;
