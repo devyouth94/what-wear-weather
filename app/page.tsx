c451c934035fdb632e84c48d4aacb2ad4c073c06
devyouth94 <devyouth94@gmail.com>
2025-03-06 17:19:10 +0900

    feat: 홈화면 로고 및 로그인유무 로직 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,59 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import LogoLarge from '~/src/components/logo-large';
+import { createClient } from '~/src/utils/supabase/client';
+
 const Home = () => {
-  return <div>Home</div>;
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    (async () => {
+      try {
+        const supabase = createClient();
+        const { data } = await supabase.auth.getUser();
+
+        timer = setTimeout(() => {
+          setIsLoading(true);
+
+          if (data.user) {
+            router.push('/weather');
+          } else {
+            router.push('/login');
+          }
+        }, 2000);
+      } catch (error) {
+        console.log('에러가 발생했어요. 로그인 페이지로 이동합니다.: ', error);
+
+        timer = setTimeout(() => {
+          router.push('/login');
+        }, 2000);
+      }
+    })();
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [router]);
+
+  return (
+    <>
+      {isLoading && (
+        <div className="absolute inset-0 z-10 flex items-center justify-center bg-black/30">
+          <span>Loading...</span>
+        </div>
+      )}
+
+      <div className="flex h-dvh w-full items-center justify-center">
+        <LogoLarge />
+      </div>
+    </>
+  );
 };
 
 export default Home;
