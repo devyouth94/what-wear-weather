0702857a3e2a1bc5a98b153d588e8cf9025e56e7
devyouth94 <devyouth94@gmail.com>
2025-03-19 13:40:30 +0900

    refactor: 날씨 페이지에 작업한 컴포넌트 적용

--- app/(main)/weather/page.tsx
+++ app/(main)/weather/page.tsx
@@ -1,53 +1,31 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 
-import {
-  getCurrentForecast,
-  getCurrentWeather,
-  type GetCurrentWeatherResponse,
-  type GetForecastResponse,
-} from '~/src/apis/weather';
+import CurrentWeather from '~/src/components/weather/current-weather';
+import Forecast from '~/src/components/weather/forecast';
 import useGeolocation from '~/src/hooks/use-geolocation';
 
 const Weather = () => {
-  const { latAndLng } = useGeolocation();
+  const { isLoading, latAndLng } = useGeolocation();
+  const baseDate = format(new Date(), 'yyyy-MM-dd HH:mm');
 
-  const [currentWeather, setCurrentWeather] =
-    useState<GetCurrentWeatherResponse | null>(null);
-  const [forecast, setForecast] = useState<GetForecastResponse | null>(null);
+  if (isLoading) {
+    return <>Loading...</>;
+  }
 
-  useEffect(() => {
-    if (!latAndLng) return;
+  const { latitude, longitude } = latAndLng!;
 
-    const baseDate = format(new Date(), 'yyyy-MM-dd HH:mm');
-
-    (async () => {
-      const data = await getCurrentWeather(
-        latAndLng.latitude,
-        latAndLng.longitude,
-        baseDate,
-      );
-
-      setCurrentWeather(data);
-    })();
-
-    (async () => {
-      const data = await getCurrentForecast(
-        latAndLng.latitude,
-        latAndLng.longitude,
-        baseDate,
-      );
-
-      setForecast(data);
-    })();
-  }, [latAndLng]);
-
-  console.log('currentWeather', currentWeather);
-  console.log('forecast', forecast);
-
-  return <div>Weather</div>;
+  return (
+    <>
+      <CurrentWeather
+        latitude={latitude}
+        longitude={longitude}
+        baseDate={baseDate}
+      />
+      <Forecast latitude={latitude} longitude={longitude} baseDate={baseDate} />
+    </>
+  );
 };
 
 export default Weather;
