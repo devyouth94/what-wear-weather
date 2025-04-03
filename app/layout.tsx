6664a25f3d8084a7c2b072d6d87e8caf2de1dc3a
devyouth94 <devyouth94@gmail.com>
2025-04-03 14:29:17 +0900

    feat: 폰트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import { type PropsWithChildren } from 'react';
 import { type Metadata } from 'next';
+import localFont from 'next/font/local';
 import { Toaster } from 'sonner';
 
 import TanstackQueryProvider from '~/src/providers/tanstack-query';
+import { cn } from '~/src/utils/class-name';
 
 import '~/src/styles/globals.css';
+
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard',
+});
 
 export const metadata: Metadata = {
   title: '왓웨어웨더',
@@ -14,7 +21,7 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
     <html lang="ko">
-      <body>
+      <body className={cn(pretendard.variable, 'font-pretendard')}>
         <TanstackQueryProvider>
           <Toaster position="bottom-center" richColors closeButton />
           {children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config: Config = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)'],
+      },
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
